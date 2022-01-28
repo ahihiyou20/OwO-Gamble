d3a7af54cd0134ccef89d8f642abb77819d99ab7
ahihiyou20 <95456402+ahihiyou20@users.noreply.github.com>
2022-01-29 03:33:23 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import sys
+import subprocess
+import pkg_resources
+import os
+import time
+if os.name == 'nt':
+    required = {'discum'}
+if os.name != 'nt':
+    required = {'discum', 'simplejson'}
+installed = {pkg.key for pkg in pkg_resources.working_set}
+missing = required - installed
+if not missing:
+    print("Requirement Package Are Installed Already! You're Good To Go")
+    time.sleep(1)
+    exit()
+if missing:
+    print("Installing Requirements Package...")
+    time.sleep(1)
+    python = sys.executable
+    subprocess.check_call([python, '-m', 'pip', 'install', *missing], stdout=subprocess.DEVNULL)
+    print("Successfully Installed Requirements Package! You're Good To Go")
+    time.sleep(2)
+    exit()
