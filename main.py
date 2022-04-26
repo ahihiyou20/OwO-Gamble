39e2362b71ca2a0f4418c287d6e360944450b2f0
ahihiyou20 <95456402+ahihiyou20@users.noreply.github.com>
2022-04-26 17:09:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
+from colorama import init
+init()
 import time
 import os
-from multiprocessing import Process
-from multiprocessing import Pool
+from multiprocessing import Process, Pool
 import random
 import re
 import atexit
-if os.name == 'posix':
- try:
-  import simplejson as json
- except:
-  os.system('pip install simplejson')
-  import simplejson as json
-if os.name == 'nt':
-  import json
+import json
 try:
- import discum
+ from discum import *
 except:
- exec(open("./setup.py").read())
- import discum
-
+ import setup
+ from discum import *
 print("""\
 ░█████╗░░██╗░░░░░░░██╗░█████╗░  ░██████╗███████╗██╗░░░░░███████╗  ██████╗░░█████╗░████████╗
 ██╔══██╗░██║░░██╗░░██║██╔══██╗  ██╔════╝██╔════╝██║░░░░░██╔════╝  ██╔══██╗██╔══██╗╚══██╔══╝
@@ -34,7 +27,6 @@ class client:
   commands=[
     "t","h"
    ]
-  bet = 1000
   totalcmd = 0
   totallost = 0
   totalwon = 0
@@ -48,24 +40,16 @@ class client:
     reset = '\033[0m'
     bold = '\033[1m'
     underline = '\033[4m'
-    if os.name == "nt":
-      purple = ''
-      okblue = ''
-      okcyan = ''
-      okgreen = ''
-      warning = ''
-      fail = ''
-      reset = ''
-      bold = ''
-      underline = ''
   with open('settings.json', "r") as file:
         data = json.load(file)
         token = data["token"]
         channel = data["channel"]
+        bet = int(data["bet"])
+  current_bet = bet
   if data["token"] and data["channel"] == 'nothing':
    print(f"{color.fail} !!! [ERROR] !!! {color.reset} Please Enter Information To Continue")
    time.sleep(1)
-   exec(open("./newdata.py").read())
+   from newdata import menu
   print('=========================')
   print('|                       |')
   print(f'| [1] {color.purple}Load data         {color.reset}|')
@@ -73,21 +57,20 @@ class client:
   print(f'| [3] {color.purple}Credit            {color.reset}|')
   print('=========================')
   time.sleep(1)
-  time.sleep(1)
-choice = int(input('Enter your choice: '))
-if (choice == 1):
+choice = input('Enter your choice: ')
+if choice == "1":
       pass
-if (choice == 2):
-  exec(open("./newdata.py").read())
-if (choice == 3):
+elif choice == "2":
+      from newdata import menu
+elif choice == "3":
       print(f'{client.color.okcyan} =========Credit=========={client.color.reset}')
       print(f'{client.color.purple} [Developer] {client.color.reset} ahihiyou20')
-      time.sleep(3)
-      exit() 
-if not (choice ==1 or 2 or 3 or 4): 
- print(f'{client.color.fail} !! [ERROR] !! {client.color.reset} Wrong input!')
- time.sleep(2)
- exit()
+      time.sleep(10)
+      exit()
+else:
+     print(f'{client.color.fail} !! [ERROR] !! {client.color.reset} Wrong input!')
+     time.sleep(2)
+     exit()
 def at():
   return f'\033[0;43m{time.strftime("%d %b %Y %H:%M:%S", time.localtime())}\033[0;21m'
 bot = discum.Client(token=client.token, log=False)
@@ -104,7 +87,6 @@ def issuechecker(resp):
     if m['author']['id'] == '408785106942164992' or m['author']['username'] == 'OwO' or m['author']['discriminator'] == '8456' or m['author']['public_flags'] == '65536':
      if 'captcha' in m['content']:
       print(f'{at()}{client.color.warning} !! [CAPTCHA] !! {client.color.reset} CAPTCHA   ACTION REQUİRED')
-      time.sleep(1)
       bot.gateway.close()
      if '(2/5)' in m['content']:
       print(f'{at()}{client.color.warning} !! [CAPTCHA] !! {client.color.reset} CAPTCHA   ACTION REQUİRED (2/5)')
@@ -124,9 +106,6 @@ def issuechecker(resp):
      if 'complete your captcha to verify that you are human!' in m['content']:
       print(f'{at()}{client.color.warning} !! [CAPTCHA] !! {client.color.reset} CAPTCHA   ACTION REQUİRED')
       bot.gateway.close()
-     if 'DM' in m['content']:
-      print(f'{at()}{client.color.warning} !! [CAPTCHA] !! {client.color.reset} CAPTCHA   ACTION REQUİRED')
-      bot.gateway.close()
      if 'you currently have' in m['content']:
       issuechecker.cash = re.findall('[0-9]+', m['content'])
       print("{}You currently have: {} Cowoncy! {}".format(client.color.warning,','.join(issuechecker.cash[1::]),client.color.reset))
@@ -141,23 +120,23 @@ def check(resp):
    if m['channel_id'] == client.channel:
     if m['author']['id'] == '408785106942164992' or m['author']['username'] == 'OwO' or m['author']['discriminator'] == '8456' or m['author']['public_flags'] == '65536':
       if 'and you won' in m['content']:
-       print("{} [INFO] Won: {} Cowoncy{}".format(client.color.okgreen,client.bet + client.bet,client.color.reset))
-       client.bet = 1000
-       client.totalwon = client.totalwon + 1
+       print("{}[INFO] Won: {} Cowoncy{}".format(client.color.okgreen,client.current_bet,client.color.reset))
+       client.current_bet = client.bet
+       client.totalwon += 1
       if 'and you lost it all... :c' in m['content']:
-       print(" {} [INFO] Lost: {} Cowoncy {}".format(client.color.fail,client.bet,client.color.reset))
-       client.bet = client.bet + client.bet
-       client.totallost = client.totallost + 1
+       print("{}[INFO] Lost: {} Cowoncy {}".format(client.color.fail,client.current_bet,client.color.reset))
+       client.current_bet *= 2
+       client.totallost += 1
 def cf():
   choice = random.choice(client.commands)
-  bot.sendMessage(str(client.channel), "owo cf {} {} ".format(client.bet,choice))
-  print("{} {} [SENT] owo cf {} {} {}".format(at(),client.color.warning,client.bet,choice,client.color.reset))
-  client.totalcmd = client.totalcmd + 1
+  bot.sendMessage(str(client.channel), "owo cf {} {} ".format(client.current_bet,choice))
+  print("{} {} [SENT] owo cf {} {} {}".format(at(),client.color.warning,client.current_bet,choice,client.color.reset))
+  client.totalcmd += 1
   time.sleep(random.randint(wbm[0], wbm[1]))
   bot.sendMessage(str(client.channel), "owo cash")
   time.sleep(5)
-  if client.bet == 128000:
-    client.bet = 1000
+  if client.bet > 128000:
+    client.current_bet = client.bet
 @bot.gateway.command
 def loopie(resp):
  if resp.event.ready:
