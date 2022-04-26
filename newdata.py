aba3f56cff3e634a87d1d8eb14d5c649a6aa209a
ahihiyou20 <95456402+ahihiyou20@users.noreply.github.com>
2022-04-26 17:07:54 +0700

    Update newdata.py

--- newdata.py
+++ newdata.py
@@ -1,25 +1,53 @@
 import time
 import json
 import os
-if os.name == 'nt':
-    pass
-if os.name == 'linux':
-    import simplejson as json
-else:
-    try:
-        import json
-    except:
-        import simplejson as json
-file = open("settings.json", "r")
-data = json.load(file)
-file.close()
-data['token'] = input("Please Enter Your Account Token: ")
-data['channel'] = input("Please Enter Your Channel ID: ")
-file = open("settings.json", "w")
-json.dump(data, file)
-file.close()
-print('Successfully saved!')
-print('Please Restart The Command Prompt')
-time.sleep(2)
-exit()
-
+from sys import exit
+def menu():
+ import json
+ file = open("settings.json", "r")
+ data = json.load(file)
+ file.close()
+ print('================================')
+ print(f'| [0] Exit And Save           |')
+ print(f'| [1] Change All Settings     |')
+ print(f'| [2] Change Token            |')
+ print(f'| [3] Change Channel          |')
+ print(f'| [4] Change Bet Amount       |')
+ print('================================')
+ choice = input("Enter Your Choice: ")
+ if choice == "0":
+  raise SystemExit
+ if choice == "1":
+  t(data,"True")
+  c(data,"True")
+  be(data,"True")
+ if choice == "2":
+  t(data,"False")
+ if choice == "3":
+  c(data,"False")
+ if choice == "4":
+  bet(data,"False")
+def t(data,all):
+ data['token'] = input("Please Enter Your Account Token: ")
+ file = open("settings.json", "w")
+ json.dump(data, file)
+ file.close()
+ print('Successfully saved!')
+ if not all == "True":
+  menu()
+def c(data,all):
+ data['channel'] = input("Please Enter Your Channel ID: ")
+ file = open("settings.json", "w")
+ json.dump(data, file)
+ file.close()
+ print('Successfully saved!')
+ if not all == "True":
+  menu()
+def bet(data,all):
+ data['bet'] = input("Enter Your Bet Amount (Must Be Integer): ")
+ file = open("settings.json", "w")
+ json.dump(data, file)
+ file.close()
+ print('Successfully saved!')
+ if not all == "True":
+  menu()
